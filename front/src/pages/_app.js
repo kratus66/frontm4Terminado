b1522287de0058aca26ec0289dca6a6af554551f
kratus66 <diegoherrera1685@hotmail.com>
2024-10-01 17:50:57 -0500

    proyecto terminado 4

--- front/src/pages/_app.js
+++ front/src/pages/_app.js
@@ -1,13 +1,18 @@
 import '../components/Layout/index.css';
-
 import Layout from '../components/Layout/Layout';
-
+import Head from 'next/head';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Head>
+        {/* Enlace al favicon */}
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
 
